facb5e974faac00fcb509ad1560392566df017a4
chadittya <chadittya@gmail.com>
2022-11-13 09:21:00 +0700

    menambahkan materi event bubling dan mengefektifkan event close pada card

--- script.js
+++ script.js
@@ -6,15 +6,34 @@
 // });
 
 // 2. dom transversal
-const btnClose = document.querySelectorAll(".close");
-btnClose.forEach(function (btn) {
-  btn.addEventListener("click", function (ec) {
-    // btn.parentElement.style.display = "none"; //dari button close[i](btn) ambil parentnya dan set style displaynya jado = none
-    // atau
-    ec.target.parentElement.style.display = "none"; //karena dari variabel ec nantinya bisa mendapatkan informasi macam2
-    ec.preventDefault();
-  });
-});
+// const btnClose = document.querySelectorAll(".close");
+// btnClose.forEach(function (btn) {
+//   btn.addEventListener("click", function (ec) {
+//     // btn.parentElement.style.display = "none"; //dari button close[i](btn) ambil parentnya dan set style displaynya jado = none
+//     // atau
+//     ec.target.parentElement.style.display = "none"; //karena dari variabel ec nantinya bisa mendapatkan informasi macam2
+//     ec.preventDefault();
+//     ec.stopPropagation();
+//   });
+// });
+
+// const cards = document.querySelectorAll(".card");
+// cards.forEach(function (card) {
+//   card.addEventListener("click", function (e) {
+//     alert("ok");
+//   });
+// });
 
 // const nama = document.querySelector(".nama");
 // console.log(nama.nextElementSibling);
+
+// 3. efektif untuk dinamyc card
+const container = document.querySelector(".container");
+container.addEventListener("click", function (e) {
+  // console.log(e.target); //js tau apa yang kita click disini, ini yang kita manfaatkan
+  if (e.target.className == "close") {
+    e.target.parentElement.style.display = "none";
+    e.preventDefault();
+    e.stopPropagation();
+  }
+});
